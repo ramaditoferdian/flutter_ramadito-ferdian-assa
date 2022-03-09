06b760e8ead10f9b99e2da85ae7d5e2823d1fb48
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2022-03-09 13:32:38 +0700

    update section 9

--- 9_Dart Object Oriented Programming 1/praktikum/oop1/bin/mobil.dart
+++ 9_Dart Object Oriented Programming 1/praktikum/oop1/bin/mobil.dart
@@ -2,10 +2,20 @@ import 'hewan.dart';
 
 class Mobil {
   var muatan = [];
+  final double kapasitas = 30;
 
   void tambahMuatan(Hewan hewan) {
-    if (muatan.length <= 10) {
+    var sumOfMuatan = totalMuatan();
+
+    var slot = kapasitas - sumOfMuatan;
+
+    if (hewan.berat <= slot) {
       muatan.add(hewan.berat);
+
+      print('Hewan [${hewan.berat}] berhasil ditambahkan');
+    } else {
+      print(
+          'Kapasitas mobil penuh !, hewan [${hewan.berat}] gagal ditambahkan');
     }
   }
 
--- 9_Dart Object Oriented Programming 1/praktikum/oop1/bin/oop1.dart
+++ 9_Dart Object Oriented Programming 1/praktikum/oop1/bin/oop1.dart
@@ -4,14 +4,17 @@ import 'mobil.dart';
 void main(List<String> arguments) {
   var h1 = Hewan();
   var h2 = Hewan();
+  var h3 = Hewan();
 
   h1.berat = 9;
   h2.berat = 8;
+  h3.berat = 20;
 
   var mobil = Mobil();
 
   mobil.tambahMuatan(h1);
   mobil.tambahMuatan(h2);
+  mobil.tambahMuatan(h3);
 
   print(mobil.muatan);
   print(mobil.totalMuatan());
