0a505c561e77ed4cadb2f227ddc146f81e23cb42
Ramadito Ferdian Assa <ramaditoferdian@gmail.com>
2022-04-11 13:53:37 +0700

    update main.dart

--- 22_FLutter Animation/praktikum/flutter_animation/lib/main.dart
+++ 22_FLutter Animation/praktikum/flutter_animation/lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
       ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        initialRoute: '/home-page',
+        initialRoute: '/',
         routes: {
           '/': (context) => Task1(),
           '/home-page': (context) => HomePage(),
